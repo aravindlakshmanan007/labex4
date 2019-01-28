afe09f5fd69f148aa5c0147dfec0fb6ecda9e537
aravind <aravindlakshmanan007@gmail.com>
2019-01-28 10:02:46 +0530

    Initial Commit

---
+++ labex4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  labex4
+//
+//  Created by Aravind on 28/01/19.
+//  Copyright © 2019 Aravind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
